ffa6915241cc9a75875c85f73dfeba0237198db8
Lennon Reid <lenonrade@163.com>
2023-11-23 10:30:32 +0800

    chore: update to ionic@7.5.6 and use standalone approach

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { IonicModule, Platform } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 import { App, AppInfo } from '@capacitor/app';
+import { IonContent, IonHeader, IonTitle, IonToolbar, Platform } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent],
 })
 export class Tab1Page {
   appInfo?: AppInfo;
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,19 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonButton, IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonButton,
+    IonIcon,
+    IonLabel
+  ],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
